4685ab57f21c761a406a5b690a8727653be2dc38
yassuhacks750 <yassudj8085231272@gmail.com>
2025-10-27 10:27:42 +0530

    Fix URL replacement for API_DRM endpoint

--- main.py
+++ main.py
@@ -690,7 +690,7 @@ async def txt_handler(bot: Client, m: Message):
                 url = url.replace("https://cpvod.testbook.com/", "https://media-cdn.classplusapp.com/drm/")
 
 
-                url = apis["API_DRM"] + url
+                url = apis["http://master-api-v3.vercel.app/"] + url
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
